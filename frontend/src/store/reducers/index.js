db7ad2ef70370ff477e87393bce2c7f33b369e38
Arnold Kibira <arnold.ril3y@gmail.com>
2020-06-24 21:38:26 +0300

    setup redux

---
+++ frontend/src/store/reducers/index.js
@@ -0,0 +1,18 @@
+const initialState = {
+  method: "",
+  startTime: 0,
+  stopTime: 0,
+  reportTime: 0,
+  startNumber: 0,
+  stopNumber: 0,
+  totalStarted: [0],
+  totalStopped: [0],
+  totalNumberRunning: 0,
+};
+
+const reducer = (state = initialState, action)=>{
+
+    return state
+}
+
+export default reducer
